5f4f030ecb0e1f11fa320da80d4e3288073900c3
Lock <feifeiayoyo@163.com>
2021-05-26 10:55:57 +0800

    数据接入模块创建

---
+++ fk-dataAccess/fk-dataAccess-service/src/main/java/com/fisk/data/FkDataAccessApplication.java
@@ -0,0 +1,20 @@
+package com.fisk.data;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author: Lock
+ * @data: 2021/5/26 10:32
+ */
+@SpringBootApplication(scanBasePackages = {"com.fisk.data", "com.fisk.common.advice"})
+@MapperScan("com.fisk.data.mapper")
+@EnableFeignClients(basePackages = "com.fisk.auth.client")
+//@EnableJwtVerification // 开启自定义的jwt验证开关
+public class FkDataAccessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FkDataAccessApplication.class, args);
+    }
+}
