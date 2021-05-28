805646b1ec494923088d3e267eec7fed7d2c3211
宇 郭 <guoyu@fisksoft.com>
2021-05-28 14:29:54 +0800

    新增： 获取sqlserver数据域（默认读表和字段）

--- fisk-chartvisual/fisk-chartvisual-service/src/main/java/com/fisk/chartvisual/util/dscon/UseSqlServerDataBase.java
+++ fisk-chartvisual/fisk-chartvisual-service/src/main/java/com/fisk/chartvisual/util/dscon/UseSqlServerDataBase.java
@@ -16,14 +16,33 @@ public class UseSqlServerDataBase extends AbstractUseDataBase {
     @Override
     public String buildDataDomainQuery(String dbName) {
         StringBuilder str = new StringBuilder();
-        str.append("SELECT ");
-        str.append("a.name as tableName, ");
-        str.append("b.name as columnName ");
+        str.append("SELECT DISTINCT ");
+        str.append("objects.name as tableName, ");
+        str.append("props.value as tableDetails, ");
+        str.append("col.columnName, ");
+        str.append("col.columnDetails ");
         str.append("FROM ");
-        str.append("sysobjects a ");
-        str.append("INNER JOIN syscolumns b ON a.id = b.id ");
-        str.append("WHERE ");
-        str.append("a.xtype= 'u' ");
+        str.append("syscolumns columns ");
+        str.append("LEFT JOIN systypes types ON columns.xusertype= types.xusertype ");
+        str.append("INNER JOIN sysobjects objects ON columns.id= objects.id  ");
+        str.append("AND objects.xtype= 'U'  ");
+        str.append("AND objects.name<> 'dtproperties' ");
+        str.append("LEFT JOIN syscomments comments ON columns.cdefault= comments.id ");
+        str.append("LEFT JOIN sys.extended_properties pro ON columns.id= pro.major_id  ");
+        str.append("AND columns.colid= pro.minor_id ");
+        str.append("LEFT JOIN sys.extended_properties props ON objects.id= props.major_id  ");
+        str.append("AND props.minor_id= 0 ");
+        str.append("LEFT JOIN ( ");
+        str.append("SELECT ");
+        str.append("tables.name AS table_name, ");
+        str.append("columns.name AS columnName, ");
+        str.append("props.value AS columnDetails  ");
+        str.append("FROM ");
+        str.append("sys.tables tables ");
+        str.append("INNER JOIN sys.columns columns ON columns.object_id = tables.object_id ");
+        str.append("LEFT JOIN sys.extended_properties props ON props.major_id = columns.object_id ");
+        str.append("AND props.minor_id = columns.column_id  ");
+        str.append(") col ON col.table_name = objects.name ");
         return str.toString();
     }
 }
