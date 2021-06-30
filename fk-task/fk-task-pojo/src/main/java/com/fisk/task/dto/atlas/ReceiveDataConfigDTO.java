5797352100264140daf3cfe40b9a489201f9d9dd
yhxu <yhxu@fisksoft.com>
2021-06-30 16:53:57 +0800

    yml add dorisconstr

--- fk-task/fk-task-pojo/src/main/java/com/fisk/task/dto/atlas/ReceiveDataConfigDTO.java
+++ fk-task/fk-task-pojo/src/main/java/com/fisk/task/dto/atlas/ReceiveDataConfigDTO.java
@@ -1,8 +1,10 @@
 package com.fisk.task.dto.atlas;
 
+import com.fisk.task.dto.MQBaseDTO;
+
 /**
  * @author Denny
  */
-public class ReceiveDataConfigDTO {
+public class ReceiveDataConfigDTO extends MQBaseDTO {
     public String id;
 }
--- fk-task/fk-task-service/src/main/java/com/fisk/task/consumer/atlas/BuildAtlasTaskListener.java
+++ fk-task/fk-task-service/src/main/java/com/fisk/task/consumer/atlas/BuildAtlasTaskListener.java
@@ -1,20 +1,28 @@
 package com.fisk.task.consumer.atlas;
 import com.alibaba.fastjson.JSON;
+import com.fisk.common.constants.MQConstants;
 import com.fisk.task.dto.atlas.ReceiveDataConfigDTO;
 import com.fisk.task.extend.aop.MQConsumerLog;
 import com.fisk.task.utils.WsSessionManager;
 import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
 /**
- * @author Denny 
+ * @
  */
+@Component
+@RabbitListener(queues = MQConstants.QueueConstants.BUILD_ATLAS_FLOW)
+@Slf4j
+
 public class BuildAtlasTaskListener {
     @RabbitHandler
     @MQConsumerLog
     public void msg(String settingid, Channel channel, Message message) {
         ReceiveDataConfigDTO dto = JSON.parseObject(settingid, ReceiveDataConfigDTO.class);
-        String A=";";
+
     }
 }
