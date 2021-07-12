a9ba76b628556f0b677b874cf18d3339156c3c91
yhxu <yhxu@fisksoft.com>
2021-07-12 11:14:58 +0800

    调整api参数

---
+++ fk-task/fk-task-pojo/src/main/java/com/fisk/task/dto/atlas/AtlasEntityDTO.java
@@ -0,0 +1,22 @@
+package com.fisk.task.dto.atlas;
+
+import com.fisk.task.dto.MQBaseDTO;
+import lombok.Data;
+
+/**
+ * @author: DennyHui
+ * CreateTime: 2021/7/9 17:34
+ * Description:
+ */
+@Data
+public class AtlasEntityDTO extends MQBaseDTO {
+    public String appid;
+    public String appName;
+    public String driveType;
+    public String createUser;
+    public String appDes;
+    public String host;
+    public String port;
+    public String dbName;
+    public String dbId;
+}
--- fk-task/fk-task-service/src/main/java/com/fisk/task/consumer/atlas/BuildAtlasInstanceTaskListener.java
+++ fk-task/fk-task-service/src/main/java/com/fisk/task/consumer/atlas/BuildAtlasInstanceTaskListener.java
@@ -2,15 +2,24 @@ package com.fisk.task.consumer.atlas;
 
 import com.alibaba.fastjson.JSON;
 import com.fisk.common.constants.MqConstants;
-import com.fisk.task.dto.atlas.AtlasEntityRdbmsDTO;
+import com.fisk.common.entity.BusinessResult;
+import com.fisk.task.dto.atlas.AtlasEntityDTO;
 import com.fisk.task.service.IAtlasBuildInstance;
 import com.rabbitmq.client.Channel;
+import fk.atlas.api.model.EntityProcess;
+import fk.atlas.api.model.EntityRdbmsDB;
+import fk.atlas.api.model.EnttityRdbmsInstance;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author: DennyHui
@@ -24,10 +33,79 @@ public class BuildAtlasInstanceTaskListener {
 
     @Resource
     IAtlasBuildInstance atlas;
-    public void msg(String dataInfo, Channel channel, Message message){
-        AtlasEntityRdbmsDTO ad=JSON.parseObject(dataInfo, AtlasEntityRdbmsDTO.class);
-        atlas.atlasBuildInstance(ad.entityInstance);
-        atlas.atlasBuildDb(ad.entityDb);
-        atlas.atlasBuildProcess(ad.entityProcess);
+
+    public void msg(String dataInfo, Channel channel, Message message) {
+        AtlasEntityDTO ae = JSON.parseObject(dataInfo, AtlasEntityDTO.class);
+        //设置日期格式
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //region  创建实例
+        EnttityRdbmsInstance.attributes_rdbms_instance ari = new EnttityRdbmsInstance.attributes_rdbms_instance();
+        EnttityRdbmsInstance.attributes_field_rdbms_instance arif = new EnttityRdbmsInstance.attributes_field_rdbms_instance();
+        arif.qualifiedName = ae.appName + "@atlas_instance";
+        arif.name = ae.appName;
+        arif.rdbms_type = ae.driveType;
+        arif.platform = "windows";
+        arif.hostname = ae.host;
+        arif.port = ae.port;
+        arif.protocol = ae.driveType + " protocal";
+        arif.contact_info = "your contact info";
+        arif.description = ae.appDes;
+        arif.owner = ae.createUser;
+        arif.ownerName = ae.createUser;
+        ari.attributes = arif;
+        EnttityRdbmsInstance.entity_rdbms_instance eri = new EnttityRdbmsInstance.entity_rdbms_instance();
+        eri.entity = ari;
+        BusinessResult insRes = atlas.atlasBuildInstance(eri);
+        //endregion
+        //region 创建DB
+        EntityRdbmsDB.entity_rdbms_db rdbms_db = new EntityRdbmsDB.entity_rdbms_db();
+        EntityRdbmsDB.attributes_rdbms_db attributes_rdbms_db = new EntityRdbmsDB.attributes_rdbms_db();
+        EntityRdbmsDB.attributes_field_rdbms_db attributes_field_rdbms_db = new EntityRdbmsDB.attributes_field_rdbms_db();
+        EntityRdbmsDB.instance_rdbms_db instance_rdbms_db = new EntityRdbmsDB.instance_rdbms_db();
+        instance_rdbms_db.guid = UUID.randomUUID().toString();
+        instance_rdbms_db.entityStatus = "ACTIVE";
+        attributes_field_rdbms_db.owner = ae.createUser;
+        attributes_field_rdbms_db.ownerName = ae.createUser;
+        attributes_field_rdbms_db.name = ae.dbName;
+        attributes_field_rdbms_db.qualifiedName = ae.dbName + "@atlas_db";
+        attributes_field_rdbms_db.displayText = ae.dbName;
+        attributes_field_rdbms_db.description = "";
+        attributes_field_rdbms_db.instance = instance_rdbms_db;
+        attributes_rdbms_db.attributes = attributes_field_rdbms_db;
+        rdbms_db.entity = attributes_rdbms_db;
+        atlas.atlasBuildDb(rdbms_db);
+        //endregion
+        //region 创建实例与数据库的连接
+        EntityProcess.entity_rdbms_process entity_rdbms_process = new EntityProcess.entity_rdbms_process();
+        List<EntityProcess.attributes_rdbms_process> earps = new ArrayList<>();
+        EntityProcess.attributes_rdbms_process attributes_rdbms_process = new EntityProcess.attributes_rdbms_process();
+        EntityProcess.attributes_field_rdbms_process attributes_field_rdbms_process = new EntityProcess.attributes_field_rdbms_process();
+        List<EntityProcess.entity> inputs = new ArrayList<>();
+        List<EntityProcess.entity> outputs = new ArrayList<>();
+        EntityProcess.entity inputentity = new EntityProcess.entity();
+        EntityProcess.entity ouputentity = new EntityProcess.entity();
+        inputentity.guid = insRes.data.toString();
+        inputentity.typeName = "rdbms_instance";
+        inputs.add(inputentity);
+        ouputentity.guid = ae.dbId;
+        ouputentity.typeName = "rdbms_db";
+        outputs.add(ouputentity);
+        attributes_field_rdbms_process.owner = ae.createUser;
+        attributes_field_rdbms_process.ownerName = ae.createUser;
+        attributes_field_rdbms_process.name = ae.appName + "_instance_process_db_" + ae.dbName;
+        attributes_field_rdbms_process.qualifiedName = ae.appName + "_instance_process_db_" + ae.dbName + "@atlas";
+        attributes_field_rdbms_process.contact_info = "";
+        attributes_field_rdbms_process.description = ae.appName + " process " + ae.dbName;
+        attributes_field_rdbms_process.createTime = df.format(new Date());
+        attributes_field_rdbms_process.updateTime = df.format(new Date());
+        attributes_field_rdbms_process.comment = "";
+        attributes_field_rdbms_process.type = "instance";
+        attributes_field_rdbms_process.inputs = inputs;
+        attributes_field_rdbms_process.outputs = outputs;
+        attributes_rdbms_process.attributes = attributes_field_rdbms_process;
+        earps.add(attributes_rdbms_process);
+        entity_rdbms_process.entities = earps;
+        atlas.atlasBuildProcess(entity_rdbms_process);
+        //endregion
     }
 }
