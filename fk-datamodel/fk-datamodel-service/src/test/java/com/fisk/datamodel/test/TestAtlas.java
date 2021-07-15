ab5720b0164b381633a06b35896f2bb71fe0adfe
Lock <feifeiayoyo@163.com>
2021-07-15 18:32:45 +0800

    bug修复:feign接口

--- fk-datamodel/fk-datamodel-service/src/test/java/com/fisk/datamodel/test/TestAtlas.java
+++ fk-datamodel/fk-datamodel-service/src/test/java/com/fisk/datamodel/test/TestAtlas.java
@@ -2,7 +2,6 @@ package com.fisk.datamodel.test;
 
 import com.fisk.common.response.ResultEntity;
 import com.fisk.dataaccess.client.DataAccessClient;
-import com.fisk.dataaccess.dto.AtlasAccessDTO;
 import com.fisk.task.dto.atlas.AtlasEntityDTO;
 import com.fisk.task.dto.atlas.AtlasEntityDbTableColumnDTO;
 import org.junit.Test;
@@ -41,15 +40,18 @@ public class TestAtlas {
     @Test
     public void test02() {
 
-        AtlasAccessDTO dto = new AtlasAccessDTO();
-        dto.appid = 6;
-        dto.tableId = 1;
-        dto.userId = "47";
-        dto.tableName = "doris_tb_test";
-        dto.atlasTableId = "1";
-        dto.dorisSelectSqlStr = "select * from doris_tb_test";
+//        AtlasAccessDTO dto = new AtlasAccessDTO();
+//        dto.appid = 6;
+//        dto.tableId = 1;
+//        dto.userId = "47";
+//        dto.tableName = "doris_tb_test";
+//        dto.atlasTableId = "1";
+//        dto.dorisSelectSqlStr = "select * from doris_tb_test";
+//
+//        client.addAtlasTableIdAndDorisSql(dto);
 
-        client.addAtlasTableIdAndDorisSql(dto);
+        ResultEntity<AtlasEntityDbTableColumnDTO> dto = client.getAtlasBuildTableAndColumn(1, 6);
+        System.out.println(dto);
 
     }
 
