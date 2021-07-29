b53e8218409e4b3f5f632bdb852ad723b1030dea
宇 郭 <guoyu@fisksoft.com>
2021-07-29 13:52:05 +0800

    feign bug更新

--- fk-common/src/main/java/com/fisk/common/feign/FeignConfig.java
+++ fk-common/src/main/java/com/fisk/common/feign/FeignConfig.java
@@ -9,13 +9,18 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * @author gy
+ */
 @Component
 public class FeignConfig implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
-        //添加token
-        requestTemplate.header(SystemConstants.HTTP_HEADER_AUTH, request.getHeader(SystemConstants.HTTP_HEADER_AUTH));
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            //添加token
+            requestTemplate.header(SystemConstants.HTTP_HEADER_AUTH, request.getHeader(SystemConstants.HTTP_HEADER_AUTH));
+        }
     }
 }
