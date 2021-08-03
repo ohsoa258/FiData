8101c40ba9a962364321de67984dc535550c9704
宇 郭 <guoyu@fisksoft.com>
2021-08-03 11:09:42 +0800

    多数据源连接修改

--- fk-task/fk-task-service/src/test/java/task/doris.java
+++ fk-task/fk-task-service/src/test/java/task/doris.java
@@ -1,41 +1,56 @@
 package task;
 
 import com.baomidou.dynamic.datasource.annotation.DS;
+import com.fisk.task.FkTaskApplication;
 import com.fisk.task.entity.TBETLlogPO;
 import com.fisk.task.mapper.TBETLLogMapper;
 import org.junit.Test;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: DennyHui
  * CreateTime: 2021/8/2 18:04
  * Description:
  */
-@MapperScan("com.fisk.task.mapper")
-@DS("datainputdb")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = FkTaskApplication.class)
+@RunWith(SpringRunner.class)
+@Component
 public class doris {
 
-    @Autowired
+    @Resource
     private TBETLLogMapper logMapper;
+    @Resource
+    private JdbcTemplate template;
 
     @Test
-   public void contextLoads() {
+    public void contextLoads() {
         System.out.println(("----- selectAll method test ------"));
         List<TBETLlogPO> loglist = logMapper.selectList(null);
         //Assert.assertEquals(5, userList.size());
         loglist.forEach(System.out::println);
     }
 
-//    public static void main(String[] args) {
-//        UpdateLogAndImportDataDTO inpData = JSON.parseObject("{\"code\":\"a25396f3-f112-11eb-bfeb-0242ac110005\"}", UpdateLogAndImportDataDTO.class);
-//        //etlmapper.update(inpData)
-//        TBETLlogPO modeletllog = inpData.toEntity(TBETLlogPO.class);
-//        modeletllog.setStatus(2);
-//        UpdateWrapper<TBETLlogPO> updateWrapper=new UpdateWrapper<>();
-//        updateWrapper.eq("code",inpData.code);
-//        int updateres=etlmapper.update(modeletllog,updateWrapper);
-//    }
+    @DS("datainputdb")
+    @Test
+    public void dorisQuery() {
+        List<TBETLlogPO> loglist = logMapper.selectList(null);
+        //Assert.assertEquals(5, userList.size());
+        loglist.forEach(System.out::println);
+    }
+
+
+    @DS("dorisdb")
+    @Test
+    public void jdbcTemp() {
+        List<Map<String, Object>> list = template.queryForList("select * from non_ods_tb_nonrealtime_db013265562579919565 limit 10");
+        System.out.println(list.toString());
+    }
 }
