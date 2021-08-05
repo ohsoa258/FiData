eb2cb758291cbb2a2086d69a565a5bf4aa8a6be8
yanwang <Password01!>
2021-08-05 15:24:38 +0800

    swagger controller添加名字

---
+++ fisk-dataservice/fisk-dataservice-service/src/main/java/com/fisk/dataservice/entity/GbfPO.java
@@ -0,0 +1,19 @@
+package com.fisk.dataservice.entity;
+
+import lombok.Data;
+
+/**
+ * @author WangYan
+ * @date 2021/8/5 11:01
+ */
+@Data
+public class GbfPO {
+    private String tableName;
+    private String id;
+    private String studentNo;
+    private String name;
+    private String age;
+    private String height;
+    private String weight;
+    private String gender;
+}
