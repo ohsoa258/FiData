1b86424195f743864cddbc7880e0fcb6f66b9782
Lock <feifeiayoyo@163.com>
2021-09-04 18:23:05 +0800

    新增: 数据查看筛选器

--- fk-dataAccess/fk-dataAccess-service/src/main/java/com/fisk/dataaccess/service/ITableFields.java
+++ fk-dataAccess/fk-dataAccess-service/src/main/java/com/fisk/dataaccess/service/ITableFields.java
@@ -1,10 +1,19 @@
 package com.fisk.dataaccess.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.fisk.dataaccess.dto.datareview.DataReviewQueryDTO;
 import com.fisk.dataaccess.entity.TableFieldsPO;
+import com.fisk.dataaccess.vo.datareview.DataReviewVO;
 
 /**
  * @author Lock
  */
 public interface ITableFields extends IService<TableFieldsPO> {
+    /**
+     * 分页
+     * @param query query
+     * @return 分页结果
+     */
+    Page<DataReviewVO> listData(DataReviewQueryDTO query);
 }
