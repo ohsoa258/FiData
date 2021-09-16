23f2ce818567c1313bbbeb6d4711d4bbfb13b07a
yanwang <Password01!>
2021-09-16 11:30:38 +0800

    新增获取关联关系接口

--- fisk-dataservice/fisk-dataservice-service/src/main/java/com/fisk/dataservice/service/ITableName.java
+++ fisk-dataservice/fisk-dataservice-service/src/main/java/com/fisk/dataservice/service/ITableName.java
@@ -1,10 +1,13 @@
 package com.fisk.dataservice.service;
 
 import com.fisk.common.response.ResultEntity;
+import com.fisk.dataservice.dto.InputParameterDTO;
+import com.fisk.dataservice.dto.OutParameterDTO;
 import com.fisk.dataservice.dto.TableDataDTO;
 import com.fisk.dataservice.enums.DataDoFieldTypeEnum;
 
 import java.sql.ResultSet;
+import java.util.List;
 
 /**
  * @author Lock
@@ -32,4 +35,11 @@ public interface ITableName {
      * @return
      */
     ResultEntity<String> getDimensionName(Integer relationId);
+
+    /**
+     *
+     * @param dto
+     * @return
+     */
+    List<OutParameterDTO> getRelationShip(InputParameterDTO dto);
 }
