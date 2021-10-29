1124987ec8a1d8c82c8f39e759e217e8aee8df12
Lock <feifeiayoyo@163.com>
2021-10-29 11:51:17 +0800

    添加: 获取所有物理表 id&name

---
+++ fk-dataAccess/fk-dataAccess-pojo/src/main/java/com/fisk/dataaccess/dto/datafactory/TableIdAndNameDTO.java
@@ -0,0 +1,12 @@
+package com.fisk.dataaccess.dto.datafactory;
+
+import lombok.Data;
+
+/**
+ * @author Lock
+ */
+@Data
+public class TableIdAndNameDTO {
+    public long id;
+    public String tableName;
+}
