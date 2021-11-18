f4c7234eb0a14f71cb92fea839a1a44f507a1776
yanwang <Password01!>
2021-11-18 15:39:26 +0800

    返回值修改

--- fk-common/src/main/java/com/fisk/common/mybatis/config/BlobTypeHandler.java
+++ fk-common/src/main/java/com/fisk/common/mybatis/config/BlobTypeHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import javax.xml.bind.DatatypeConverter;
 import java.sql.*;
@@ -21,34 +22,41 @@ public class BlobTypeHandler extends BaseTypeHandler<String> {
     @Override
     public String getNullableResult(ResultSet rs, String columnName) throws SQLException {
         Blob blob = rs.getBlob(columnName);
-        if(blob==null){
+        String base64Binary = DatatypeConverter.printBase64Binary(blob.getBytes(1, (int) blob.length()));
+        if (blob == null) {
             return "";
-        }else {
-           return "data:image/jpg;base64,"+ DatatypeConverter.printBase64Binary(blob.getBytes(1, (int)blob.length()));
+        } else if (!StringUtils.isEmpty(base64Binary)) {
+            return "data:image/jpg;base64," + base64Binary;
         }
 
+        return "";
     }
 
     @Override
     public String getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
         Blob blob = rs.getBlob(columnIndex);
-        if(blob==null){
+        String base64Binary = DatatypeConverter.printBase64Binary(blob.getBytes(1, (int) blob.length()));
+        if (blob == null) {
             return "";
-        }else {
-            return  "data:image/jpg;base64,"+ DatatypeConverter.printBase64Binary(blob.getBytes(1, (int)blob.length()));
+        } else if (!StringUtils.isEmpty(base64Binary)) {
+            return "data:image/jpg;base64," + base64Binary;
 
         }
+
+        return "";
     }
 
     @Override
     public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
         Blob blob = cs.getBlob(columnIndex);
-        if(blob==null){
+        String base64Binary = DatatypeConverter.printBase64Binary(blob.getBytes(1, (int) blob.length()));
+        if (blob == null) {
             return "";
-        }else {
-            return "data:image/jpg;base64,"+ DatatypeConverter.printBase64Binary(blob.getBytes(1, (int)blob.length()));
+        } else if (!StringUtils.isEmpty(base64Binary)) {
+            return "data:image/jpg;base64," + base64Binary;
         }
 
+        return "";
     }
 
 }
