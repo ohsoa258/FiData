68b79830b720e88172ff0a8a8998b8695a4574ac
jianwen <jianwen@fisksoft.com.cn>
2021-12-01 16:35:51 +0800

    请求方式修改

--- fk-datamodel/fk-datamodel-api/src/main/java/com/fisk/datamodel/client/DataModelClient.java
+++ fk-datamodel/fk-datamodel-api/src/main/java/com/fisk/datamodel/client/DataModelClient.java
@@ -9,10 +9,7 @@ import com.fisk.dataservice.dto.isDimensionDTO;
 import com.fisk.dataservice.enums.DataDoFieldTypeEnum;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -143,7 +140,7 @@ public interface DataModelClient {
      * @param dto
      * @return
      */
-    @GetMapping("/dimension/isExistAssociate")
+    @PostMapping("/dimension/isExistAssociate")
     ResultEntity<Object> isExistAssociate(@Validated @RequestBody isDimensionDTO dto);
 
 }
