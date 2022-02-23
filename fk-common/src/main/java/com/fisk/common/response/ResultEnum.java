722e44186cebcd925c284d07dc8277ce2f331fa0
Lock <feifeiayoyo@163.com>
2022-02-23 14:15:35 +0800

    common:新增功能,数据接入新增错误状态码

--- fk-common/src/main/java/com/fisk/common/response/ResultEnum.java
+++ fk-common/src/main/java/com/fisk/common/response/ResultEnum.java
@@ -95,6 +95,7 @@ public enum ResultEnum {
     PUSH_TABLEID_NULL(5023, "推送的api_id不可为空"),
     REALTIME_ACCOUNT_OR_PWD_ERROR(5024, "请输入正确的账号或密码"),
     REALTIME_ACCOUNT_ISEXIST(5025, "当前账号已存在,请重新输入"),
+    GENERATE_PDF_ERROR(5026, "生成PDF文档失败"),
 
 
     /**
