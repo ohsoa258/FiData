91b9de3f560372b453272ac8c1629a526473b203
jianwen <jianwen@fisksoft.com.cn>
2022-03-31 18:16:12 +0800

    列级、用户组相关接口
    Signed-off-by: jianwen <jianwen@fisksoft.com.cn>

--- fisk-governance-center/fisk-governance-center-web/src/main/java/com/fisk/datagovernance/mapper/datasecurity/UserGroupInfoMapper.java
+++ fisk-governance-center/fisk-governance-center-web/src/main/java/com/fisk/datagovernance/mapper/datasecurity/UserGroupInfoMapper.java
@@ -10,6 +10,6 @@ import org.apache.ibatis.annotations.Mapper;
  * @date 2022-03-28 15:47:33
  */
 @Mapper
-public interface UsergroupInfoMapper extends FKBaseMapper<UserGroupInfoPO> {
+public interface UserGroupInfoMapper extends FKBaseMapper<UserGroupInfoPO> {
 	
 }
