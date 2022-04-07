772f0b331c5d1d398b5c2eae50c06f5ab16f056c
yanwang <yanwang@fiskcloud.com>
2022-04-07 15:38:07 +0800

    swagger扫描

--- fisk-mdm-model/fisk-mdm-model-web/src/main/java/com/fisk/mdm/controller/ModelController.java
+++ fisk-mdm-model/fisk-mdm-model-web/src/main/java/com/fisk/mdm/controller/ModelController.java
@@ -4,10 +4,12 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fisk.common.core.response.ResultEntity;
 import com.fisk.common.core.response.ResultEntityBuild;
 import com.fisk.common.core.response.ResultEnum;
+import com.fisk.mdm.config.SwaggerConfig;
 import com.fisk.mdm.service.IModelService;
 import com.fisk.mdm.dto.model.ModelDTO;
 import com.fisk.mdm.dto.model.ModelQueryDTO;
 import com.fisk.mdm.vo.model.ModelVO;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ import javax.annotation.Resource;
 /**
  * @author chenYa
  */
+@Api(tags = {SwaggerConfig.TAG_2})
 @RestController
 @RequestMapping("/model")
 public class ModelController {
