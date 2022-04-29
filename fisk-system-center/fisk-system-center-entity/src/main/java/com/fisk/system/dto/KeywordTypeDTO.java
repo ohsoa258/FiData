3d463f2e7e9c27c5a631085e1faa933e7f91964b
Lock <feifeiayoyo@163.com>
2022-04-29 16:00:15 +0800

    system: 修改,swagger提示完善

--- fisk-system-center/fisk-system-center-entity/src/main/java/com/fisk/system/dto/KeywordTypeDTO.java
+++ fisk-system-center/fisk-system-center-entity/src/main/java/com/fisk/system/dto/KeywordTypeDTO.java
@@ -15,8 +15,9 @@ public class KeywordTypeDTO {
     /**
      * SQL关键字类型
      */
-    @ApiModelProperty(value = "SQL关键字类型: (1:mysql  2:sqlserver  3:pgsql  4:doris)", required = true)
+    @ApiModelProperty(value = "SQL关键字类型: (1:mysql  2:sqlserver  3:pgsql  4:doris)")
     public List<Long> keywordType;
     @NotNull
+    @ApiModelProperty(value = "输入的字段名称")
     public String field;
 }
