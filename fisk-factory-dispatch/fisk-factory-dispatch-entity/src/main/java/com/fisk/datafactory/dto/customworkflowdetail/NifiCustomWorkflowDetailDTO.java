dd0a2553da481776b43fa06dc9892455a5195a18
Lock <feifeiayoyo@163.com>
2022-05-01 13:46:55 +0800

    dispatch: 修改,swagger注释完善

--- fisk-factory-dispatch/fisk-factory-dispatch-entity/src/main/java/com/fisk/datafactory/dto/customworkflowdetail/NifiCustomWorkflowDetailDTO.java
+++ fisk-factory-dispatch/fisk-factory-dispatch-entity/src/main/java/com/fisk/datafactory/dto/customworkflowdetail/NifiCustomWorkflowDetailDTO.java
@@ -1,5 +1,6 @@
 package com.fisk.datafactory.dto.customworkflowdetail;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -11,18 +12,23 @@ public class NifiCustomWorkflowDetailDTO {
     public long id;
     public Long pid;
     public String workflowId;
+    @ApiModelProperty(value = "左边组件类型id", required = true)
     public Integer componentsId;
+    @ApiModelProperty(value = "应用id or 业务域id")
     public String appId;
+    @ApiModelProperty(value = "表id")
     public String tableId;
     public String appName;
     public String tableName;
     /**
      * 常规: 名称
      */
+    @ApiModelProperty(value = "左边组件名称", required = true)
     public String componentName;
     /**
      * 常规: 类型
      */
+    @ApiModelProperty(value = "具体的组件名称,如数仓下的数仓维度表任务,数仓事实表任务等", required = true)
     public String componentType;
     /**
      * 常规: 描述
