97a476a01747b127c79cb863cc41b30d120c580a
Lock <feifeiayoyo@163.com>
2022-05-03 11:20:00 +0800

    access: 修改,生成ptf文档模板修改

--- fisk-framework-common/src/main/java/com/fisk/common/core/constants/ApiConstants.java
+++ fisk-framework-common/src/main/java/com/fisk/common/core/constants/ApiConstants.java
@@ -22,8 +22,8 @@ public class ApiConstants {
             "    \"standard\":\"接口采用HTTP协议，TCP连接方式。数据传输格式采用非加密的JSON格式。API请求方式为POST，文本编码格式为UTF-8；\",\n" +
             "    \"standard_query\":\"暂无。\",\n" +
             "    \"authStandard\":\"第三方系统在访问平台API时需要进行身份验证，通过调用“获取Token”接口，传递账号密码获取Token（60分钟有效期）。\",\n" +
-            "    \"uatAddress\":\"测试环境：{api_uat_address}\",\n" +
-            "    \"prdAddress\":\"生产环境：{api_prd_address}(预留地址,暂不使用)\",\n" +
+            "    \"uatAddress\":\"{api_uat_address}\",\n" +
+//            "    \"prdAddress\":\"生产环境：{api_prd_address}(预留地址,暂不使用)\",\n" +
             "    \"apiCatalogueDTOS\":[\n" +
             "        {\n" +
             "            \"grade\":1,\n" +
@@ -68,7 +68,7 @@ public class ApiConstants {
             "        {\n" +
             "            \"grade\":3,\n" +
             "            \"catalogueIndex\":\"2.3.\",\n" +
-            "            \"catalogueName\":\"环境信息\"\n" +
+            "            \"catalogueName\":\"接口地址\"\n" +
             "        },\n" +
             "        {\n" +
             "            \"grade\":3,\n" +
