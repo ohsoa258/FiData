55556f4f6bcc47a993d6f1069f843c2fb97ef066
Lock <feifeiayoyo@163.com>
2022-05-24 17:33:12 +0800

    task: 修改,注释回退

--- fisk-task-center/fisk-task-center-web/src/main/java/com/fisk/task/listener/redis/RedisKeyExpirationListeners.java
+++ fisk-task-center/fisk-task-center-web/src/main/java/com/fisk/task/listener/redis/RedisKeyExpirationListeners.java
@@ -1,6 +1,8 @@
 package com.fisk.task.listener.redis;
 
 import com.fisk.dataaccess.client.DataAccessClient;
+import com.fisk.dataaccess.dto.api.ApiImportDataDTO;
+import com.fisk.task.enums.OlapTableEnum;
 import com.fisk.task.utils.KafkaTemplateHelper;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
@@ -8,6 +10,7 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.Objects;
 
 @Component
 public class RedisKeyExpirationListeners extends KeyExpirationEventMessageListener {
@@ -28,24 +31,24 @@ public class RedisKeyExpirationListeners extends KeyExpirationEventMessageListen
      */
     @Override
     public void onMessage(Message message, byte[] pattern) {
-//        // 用户做自己的业务处理即可,注意message.toString()可以获取失效的key
-//        String expiredKey = message.toString();
-//        System.out.println("即将调用的节点:" + expiredKey);
-//        //用户key失效不做处理
-//        if (!expiredKey.toLowerCase().contains("auth")) {
-//            //此时,expiredKey就是即将要调用的节点,需要发消息topic_name就是expiredKey
-//            String[] split = expiredKey.split("\\.");
-//            String tableType = split[4];
-//            int type = Integer.parseInt(tableType);
-//            if (Objects.equals(type, OlapTableEnum.PHYSICS_API.getValue())) {
-//                ApiImportDataDTO apiImportDataDTO = new ApiImportDataDTO();
-//                apiImportDataDTO.workflowId = split[3];
-//                apiImportDataDTO.appId = Long.parseLong(split[5]);
-//                apiImportDataDTO.apiId = Long.parseLong(split[6]);
-//                dataAccessClient.importData(apiImportDataDTO);
-//            } else {
-//                kafkaTemplateHelper.sendMessageAsync(expiredKey, "即将调用的节点");
-//            }
-//        }
+        // 用户做自己的业务处理即可,注意message.toString()可以获取失效的key
+        String expiredKey = message.toString();
+        System.out.println("即将调用的节点:" + expiredKey);
+        //用户key失效不做处理
+        if (!expiredKey.toLowerCase().contains("auth")) {
+            //此时,expiredKey就是即将要调用的节点,需要发消息topic_name就是expiredKey
+            String[] split = expiredKey.split("\\.");
+            String tableType = split[4];
+            int type = Integer.parseInt(tableType);
+            if (Objects.equals(type, OlapTableEnum.PHYSICS_API.getValue())) {
+                ApiImportDataDTO apiImportDataDTO = new ApiImportDataDTO();
+                apiImportDataDTO.workflowId = split[3];
+                apiImportDataDTO.appId = Long.parseLong(split[5]);
+                apiImportDataDTO.apiId = Long.parseLong(split[6]);
+                dataAccessClient.importData(apiImportDataDTO);
+            } else {
+                kafkaTemplateHelper.sendMessageAsync(expiredKey, "即将调用的节点");
+            }
+        }
     }
 }
