f750fbe799543c1ba66922640ed3b8289de6f3a4
Lock <feifeiayoyo@163.com>
2022-06-27 17:02:56 +0800

    factory-dispatch: 新增: 新增管道DAG结构对象

---
+++ fisk-factory-dispatch/fisk-factory-dispatch-entity/src/main/java/com/fisk/datafactory/dto/tasknifi/PipeDagDTO.java
@@ -0,0 +1,20 @@
+package com.fisk.datafactory.dto.tasknifi;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author Lock
+ * @version 2.6
+ * @description 管道dag结构
+ * @date 2022/6/27 16:38
+ */
+@Data
+public class PipeDagDTO {
+
+    /**
+     * task无序集合
+     */
+    public List<NifiPortsHierarchyDTO> list;
+}
