207de176f7c22fe5f86a2c5a55b1e7c8604fc68c
Lock <feifeiayoyo@163.com>
2022-07-15 14:23:09 +0800

    factory-access: test: redis生成全局唯一ID测试

--- fisk-factory-access/fisk-factory-access-web/src/test/java/com/fisk/dataaccess/test/LogTest.java
+++ fisk-factory-access/fisk-factory-access-web/src/test/java/com/fisk/dataaccess/test/LogTest.java
@@ -2,8 +2,15 @@ package com.fisk.dataaccess.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.support.atomic.RedisAtomicLong;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: Lock
@@ -12,11 +19,40 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class LogTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
     public void test() throws InternalError {
 
-        int a = 1;
-        int b = 2;
-        System.out.println(a + b);
+        String key = prefix();
+
+
+        RedisAtomicLong redisAtomicLong = new RedisAtomicLong(key, redisTemplate.getConnectionFactory());
+        //设置起始值
+        redisAtomicLong.set(1);
+        // 设置redis步长增长为2
+        redisAtomicLong.addAndGet(5);
+        // for (int i = 0; i < 100; i++) {
+        long andIncrement = redisAtomicLong.getAndIncrement();
+        //设置5位，不够则前面补零
+        String orderId = prefix() + String.format("%1$05d", andIncrement);
+        String insertSQL = "insert into orderNumber value('" + orderId + "');";
+        // System.out.println(Thread.currentThread().getName() +
+        // ",insertSQL:" + insertSQL);
+        if ((null == redisAtomicLong || redisAtomicLong.longValue() == 0)) {// 初始设置过期时间
+            redisAtomicLong.expire(200, TimeUnit.SECONDS);
+        }
+        System.out.println(insertSQL);
+    }
+
+    public String prefix() {
+        String temp_str = "";
+        Date dt = new Date();
+        // 最后的aa表示“上午”或“下午” HH表示24小时制 如果换成hh表示12小时制
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        temp_str = sdf.format(dt);
+        return temp_str;
     }
 }
