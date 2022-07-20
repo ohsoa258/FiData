8704c394ba2d95e759f0a124d3a990aab2d10b10
fukaic@fiskcloud.com <cuifukai01!>
2022-07-20 16:17:48 +0800

    数据工厂-底层表更新逻辑

--- fisk-task-center/fisk-task-center-web/src/main/java/com/fisk/task/mapper/TaskPgTableStructureMapper.java
+++ fisk-task-center/fisk-task-center-web/src/main/java/com/fisk/task/mapper/TaskPgTableStructureMapper.java
@@ -2,9 +2,19 @@ package com.fisk.task.mapper;
 
 import com.fisk.common.framework.mybatis.FKBaseMapper;
 import com.fisk.task.entity.TaskPgTableStructurePO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 /**
  * @author JianWenYang
  */
 public interface TaskPgTableStructureMapper extends FKBaseMapper<TaskPgTableStructurePO> {
+    /**
+     * 失败修改有效版本
+     *
+     * @param version 表版本
+     * @return void
+     */
+    @Update("update tb_task_pg_table_structure set valid_version=0 where version=#{version}")
+    void updatevalidVersion(@Param("version") String version);
 }
