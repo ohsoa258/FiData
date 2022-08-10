6dc41cdcc2412381cfa19e2463d4e5318291c378
jianwen <jianwen@fisksoft.com.cn>
2022-08-10 15:56:41 +0800

    元数据获得应用、业务域名称

---
+++ fisk-framework-common/src/main/java/com/fisk/common/server/metadata/AppBusinessInfoDTO.java
@@ -0,0 +1,15 @@
+package com.fisk.common.server.metadata;
+
+import lombok.Data;
+
+/**
+ * @author JianWenYang
+ */
+@Data
+public class AppBusinessInfoDTO {
+
+    public long id;
+
+    public String name;
+
+}
