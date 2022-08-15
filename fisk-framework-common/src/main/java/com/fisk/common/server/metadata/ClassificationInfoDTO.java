a3fdc53af9dd8a621331620c6af92c20a1ea2b63
jianwen <jianwen@fisksoft.com.cn>
2022-08-15 11:46:24 +0800

    添加同步业务分类dto

---
+++ fisk-framework-common/src/main/java/com/fisk/common/server/metadata/ClassificationInfoDTO.java
@@ -0,0 +1,15 @@
+package com.fisk.common.server.metadata;
+
+import lombok.Data;
+
+/**
+ * @author JianWenYang
+ */
+@Data
+public class ClassificationInfoDTO {
+
+    public String name;
+
+    public String description;
+
+}
