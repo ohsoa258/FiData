b5e97fda73a52f95827c0862d9774151cf2fc9f5
jianwen <jianwen@fisksoft.com.cn>
2022-08-24 14:05:43 +0800

    宽表查询SQL修改

--- fisk-factory-model/fisk-factory-model-web/src/main/java/com/fisk/datamodel/service/impl/widetable/WideTableImpl.java
+++ fisk-factory-model/fisk-factory-model-web/src/main/java/com/fisk/datamodel/service/impl/widetable/WideTableImpl.java
@@ -217,7 +217,7 @@ public class WideTableImpl
     public WideTableQueryPageDTO getWideTableData(String sql, int pageSize) {
         WideTableQueryPageDTO data = new WideTableQueryPageDTO();
         try {
-            String newSql = sql.replace("", "");
+            String newSql = sql.replace("external_", "");
             Connection conn = dimensionImpl.getStatement(driver, url, userName, password);
             Statement st = conn.createStatement();
             switch (typeName.toLowerCase()) {
