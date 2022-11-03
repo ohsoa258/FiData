871ae86387decf66ab71bdf37425a6727421523e
jianwen <jianwen@fisksoft.com.cn>
2022-11-03 19:42:40 +0800

    新增数据库枚举类

---
+++ fisk-framework-common/src/main/java/com/fisk/common/core/enums/dbdatatype/PgTypeEnum.java
@@ -0,0 +1,70 @@
+package com.fisk.common.core.enums.dbdatatype;
+
+import com.fisk.common.core.enums.BaseEnum;
+
+/**
+ * @author JianWenYang
+ */
+public enum PgTypeEnum implements BaseEnum {
+
+    BIT(1, "bit"),
+    BOOL(2, "bool"),
+    BOX(3, "box"),
+    BYTEA(4, "bytea"),
+    CHAR(5, "char"),
+    CIDR(6, "cidr"),
+    CIRCLE(7, "circle"),
+    DATE(8, "date"),
+    DECIMAL(9, "decimal"),
+    FLOAT4(10, "float4"),
+    FLOAT8(12, "float8"),
+    INET(13, "inet"),
+    INT2(14, "int2"),
+    INT4(15, "int4"),
+    INT8(16, "int8"),
+    INTERVAL(17, "interval"),
+    JSON(18, "json"),
+    JSONB(19, "jsonb"),
+    LINE(20, "line"),
+    LSEG(21, "lseg"),
+    MACADDR(22, "macaddr"),
+    MONEY(23, "money"),
+    NUMERIC(24, "numeric"),
+    PATH(25, "path"),
+    POINT(26, "point"),
+    POLYGON(27, "polygon"),
+    SERIAL2(28, "serial2"),
+    SERIAL4(29, "serial4"),
+    SERIAL8(30, "serial8"),
+    TEXT(31, "text"),
+    TIME(32, "time"),
+    TIMESTAMP(33, "timestamp"),
+    TIMESTAMPtz(34, "timestamptz"),
+    TIMETZ(35, "timetz"),
+    TSQUERY(36, "tsquery"),
+    TSVECTOR(37, "tsvector"),
+    TXID_SNAPshot(38, "txid_snapshot"),
+    UUID(39, "uuid"),
+    VARBIT(40, "varbit"),
+    VARCHAR(41, "varchar"),
+    XML(42, "xml");
+
+    private final String name;
+    private final int value;
+
+    PgTypeEnum(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ fisk-framework-common/src/main/java/com/fisk/common/core/enums/dbdatatype/SqlServerTypeEnum.java
@@ -0,0 +1,65 @@
+package com.fisk.common.core.enums.dbdatatype;
+
+import com.fisk.common.core.enums.BaseEnum;
+
+/**
+ * @author JianWenYang
+ */
+public enum SqlServerTypeEnum implements BaseEnum {
+
+    /**
+     * bigint
+     */
+    BIGINT(1, "bigint"),
+    BINARY(2, "binary"),
+    BIT(3, "bit"),
+    CHAR(4, "char"),
+    DATE(5, "date"),
+    DATETIME(6, "datetime"),
+    DATETIME2(7, "datetime2"),
+    DATETIMEOFFSET(8, "datetimeoffset"),
+    DECIMAL(9, "decimal"),
+    FLOAT(10, "float"),
+    GEOGRAPHY(11, "geography"),
+    GEOMETRY(12, "geometry"),
+    HIERARCHYID(13, "hierarchyid"),
+    IMAGE(14, "image"),
+    INT(15, "int"),
+    MONEY(16, "money"),
+    NCHAR(17, "nchar"),
+    NTEXT(18, "ntext"),
+    NUMERIC(19, "numeric"),
+    NVARCHAR(20, "nvarchar"),
+    REAL(21, "real"),
+    SMALLDATETIME(22, "smalldatetime"),
+    SMALLINT(23, "smallint"),
+    SMALLMONEY(24, "smallmoney"),
+    SQL_VARIANT(25, "sql_variant"),
+    TEXT(26, "text"),
+    TIME(27, "time"),
+    TIMESTAMP(28, "timestamp"),
+    TINYINT(29, "tinyint"),
+    UNIQUEIDENTIFIER(30, "uniqueidentifier"),
+    VARBINARY(31, "varbinary"),
+    VARCHAR(32, "varchar"),
+    XML(33, "xml");
+
+    private final String name;
+    private final int value;
+
+    SqlServerTypeEnum(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+}
