6b91b5c75eb145013d77c6f93fbf725d8d435f1e
lijiawen <dick@fisksoft.com>
2022-11-09 13:53:36 +0800

    提交新建模块 license

---
+++ fisk-license-registry/fisk-license-registry-web/src/test/java/com/fisk/license/test/LicenseTest.java
@@ -0,0 +1,23 @@
+package com.fisk.license.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author dick
+ * @version 1.0
+ * @description License测试类
+ * @date 2022/11/9 13:20
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class LicenseTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
