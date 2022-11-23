f3e70ab6c6d5b5b607dc64293e7d1789969df91a
fukaic@fiskcloud.com <cuifukai01!>
2022-11-23 10:59:40 +0800

    物理表覆盖方式添加配置4

--- fisk-task-center/fisk-task-center-entity/src/main/java/com/fisk/task/po/TableNifiSettingPO.java
+++ fisk-task-center/fisk-task-center-entity/src/main/java/com/fisk/task/po/TableNifiSettingPO.java
@@ -71,11 +71,11 @@ public class TableNifiSettingPO extends BasePO {
     public String convertEndTimeToJsonProcessorId;
     public String setEndTimeProcessorId;
     //--------------------------------------------
-    public String replaceTextForFtpProcessId;
+    public String replaceTextForFtpProcessorId;
     public String invokeHttpForFtpProcessorId;
     public String queryVersionProcessorId;
     public String convertVersionToJsonProcessorId;
     public String setVersionProcessorId;
-    public String replaceTextForVersionProcessId;
+    public String replaceTextForVersionProcessorId;
     public String invokeHttpForVersionProcessorId;
 }
