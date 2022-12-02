8f2d67d2b948262dbea82000b8963ac39a51a5e0
lijiawen <dick@fisksoft.com>
2022-12-02 20:47:31 +0800

    1

--- fisk-governance-center/fisk-governance-center-entity/src/main/java/com/fisk/datagovernance/dto/dataquality/datacheck/DataCheckQueryDTO.java
+++ fisk-governance-center/fisk-governance-center-entity/src/main/java/com/fisk/datagovernance/dto/dataquality/datacheck/DataCheckQueryDTO.java
@@ -14,15 +14,15 @@ import lombok.Data;
 @Data
 public class DataCheckQueryDTO {
     /**
-     * 数据源表主键id
+     * 数据源表主键id，查询表/视图时必填
      */
-    @ApiModelProperty(value = "数据源表主键id")
+    @ApiModelProperty(value = "数据源表主键id，选中表/视图节点必填")
     public int datasourceId;
 
     /**
      * 数据源类型
      */
-    @ApiModelProperty(value = "数据源类型")
+    @ApiModelProperty(value = "数据源类型，选中节点必填")
     public SourceTypeEnum sourceTypeEnum;
 
     /**
@@ -31,18 +31,18 @@ public class DataCheckQueryDTO {
      * FiData：表ID
      * 树节点：NodeId
      */
-    @ApiModelProperty(value = "选中的标识")
+    @ApiModelProperty(value = "选中的标识，选中节点必填")
     public String uniqueId;
 
     /**
-     * 节点类型
+     * 节点类型，选中后节点类型必填
      */
-    @ApiModelProperty(value = "节点类型")
+    @ApiModelProperty(value = "节点类型，选中节点必填")
     public LevelTypeEnum levelType;
 
     /**
      * 表业务类型 1：事实表、2：维度表、3、指标表  4、宽表
      */
-    @ApiModelProperty(value = "表业务类型 1：事实表、2：维度表、3、指标表  4、宽表")
+    @ApiModelProperty(value = "表业务类型，选中表/视图节点必填")
     public int tableBusinessType;
 }
