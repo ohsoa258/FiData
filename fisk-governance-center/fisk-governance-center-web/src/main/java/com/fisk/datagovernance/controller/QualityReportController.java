4fac66e13868e848bac6e543fb1116b5f93e678e
lijiawen <dick@fisksoft.com>
2022-12-06 12:31:27 +0800

    1

--- fisk-governance-center/fisk-governance-center-web/src/main/java/com/fisk/datagovernance/controller/QualityReportController.java
+++ fisk-governance-center/fisk-governance-center-web/src/main/java/com/fisk/datagovernance/controller/QualityReportController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fisk.common.core.response.ResultEntity;
 import com.fisk.common.core.response.ResultEntityBuild;
 import com.fisk.common.core.response.ResultEnum;
+import com.fisk.common.framework.advice.ControllerAOPConfig;
 import com.fisk.datagovernance.config.SwaggerConfig;
 import com.fisk.datagovernance.dto.dataquality.qualityreport.QualityReportDTO;
 import com.fisk.datagovernance.dto.dataquality.qualityreport.QualityReportEditDTO;
@@ -92,6 +93,7 @@ public class QualityReportController {
 
     @ApiOperation("下载报告记录")
     @GetMapping("/downloadReportRecord/{reportLogId}")
+    @ControllerAOPConfig(printParams = false)
     public void downloadReportRecord(@PathVariable("reportLogId") int reportLogId, HttpServletResponse response) {
         service.downloadReportRecord(reportLogId, response);
     }
