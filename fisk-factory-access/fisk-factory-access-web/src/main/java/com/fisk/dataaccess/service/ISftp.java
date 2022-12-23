efc70b6e5b3023b6079857b1c7a4a733683f9c1f
jianwen <jianwen@fisksoft.com.cn>
2022-12-23 10:52:05 +0800

    sftp上传秘钥修改

--- fisk-factory-access/fisk-factory-access-web/src/main/java/com/fisk/dataaccess/service/ISftp.java
+++ fisk-factory-access/fisk-factory-access-web/src/main/java/com/fisk/dataaccess/service/ISftp.java
@@ -1,9 +1,9 @@
 package com.fisk.dataaccess.service;
 
 import com.fisk.common.core.response.ResultEnum;
+import com.fisk.common.core.utils.Dto.sftp.ExcelTreeDTO;
 import com.fisk.dataaccess.dto.app.DbConnectionDTO;
 import com.fisk.dataaccess.dto.ftp.ExcelDTO;
-import com.fisk.common.core.utils.Dto.sftp.ExcelTreeDTO;
 import com.fisk.dataaccess.dto.ftp.FtpPathDTO;
 import com.fisk.dataaccess.dto.sftp.SftpPreviewQueryDTO;
 import com.fisk.dataaccess.dto.sftp.SftpUploadDTO;
@@ -43,10 +43,10 @@ public interface ISftp {
     /**
      * 上传秘钥文件
      *
-     * @param appId
+     * @param abbreviationName
      * @param file
      * @return
      */
-    SftpUploadDTO uploadSecretKeyFile(Integer appId, MultipartFile file);
+    SftpUploadDTO uploadSecretKeyFile(String abbreviationName, MultipartFile file);
 
 }
