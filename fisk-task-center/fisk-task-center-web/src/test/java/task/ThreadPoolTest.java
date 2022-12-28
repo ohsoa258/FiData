1ff3320772907af4085caf1faf306523748a837b
FISKCLOUDguoyu <guoyu@fisksoft.com>
2022-12-28 12:40:31 +0800

    增加测试用例

--- fisk-task-center/fisk-task-center-web/src/test/java/task/ThreadPoolTest.java
+++ fisk-task-center/fisk-task-center-web/src/test/java/task/ThreadPoolTest.java
@@ -11,8 +11,11 @@ import org.junit.Test;
  */
 public class ThreadPoolTest {
 
+    /**
+     * 测试 任务超过上限，继续添加任务是否报错
+     */
     @Test
-    public void test() throws Exception {
+    public void test_max_limit() throws Exception {
         for (int i = 0; i < 25; i++) {
             int finalI = i;
             TaskThreadPool.taskPool.submit(() -> {
@@ -25,6 +28,38 @@ public class ThreadPoolTest {
             });
             Thread.sleep(100);
         }
+        Thread.sleep(10000);
+    }
+
+    /**
+     * 测试
+     */
+    @Test
+    public void test() throws Exception {
+        TaskThreadPool.taskPool.submit(() -> {
+            System.out.printf("任务：%s，线程id：{%s}%n", 1, Thread.currentThread().getId());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        TaskThreadPool.taskPool.submit(() -> {
+            System.out.printf("任务：%s，线程id：{%s}%n", 2, Thread.currentThread().getId());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        TaskThreadPool.taskPool.submit(() -> {
+            System.out.printf("任务：%s，线程id：{%s}%n", 3, Thread.currentThread().getId());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
         Thread.sleep(10000);
     }
 }
