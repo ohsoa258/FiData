da3c98ff60deed6cc47dd80f0db5395759ea7fc7
jianwen <jianwen@fisksoft.com.cn>
2022-12-28 16:49:21 +0800

    注释kafka元数据推送

--- fisk-task-center/fisk-task-center-web/src/main/java/com/fisk/task/listener/metadata/impl/BuildMetaDataListener.java
+++ fisk-task-center/fisk-task-center-web/src/main/java/com/fisk/task/listener/metadata/impl/BuildMetaDataListener.java
@@ -1,11 +1,8 @@
 package com.fisk.task.listener.metadata.impl;
 
-import com.alibaba.fastjson.JSONObject;
 import com.fisk.common.core.response.ResultEnum;
 import com.fisk.datamanage.client.DataManageClient;
-import com.fisk.task.dto.task.BuildMetaDataDTO;
 import com.fisk.task.listener.metadata.IMetaDataListener;
-import com.fisk.task.server.TaskThreadPool;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
@@ -25,17 +22,16 @@ public class BuildMetaDataListener implements IMetaDataListener {
     @Override
     public ResultEnum metaData(String dataInfo, Acknowledgment ack) {
         try {
-            log.info("元数据实时同步数据:{}", dataInfo);
+            /*log.info("元数据实时同步数据:{}", dataInfo);
             TaskThreadPool.TASK_POOL.submit(() -> {
                 try {
                     log.info("*****开始推送元数据实时同步数据*****");
                     BuildMetaDataDTO data = JSONObject.parseObject(dataInfo, BuildMetaDataDTO.class);
-                    log.info("*****开始推送元数据实时同步数据1*****");
                     dataManageClient.consumeMetaData(data.data);
                 } catch (Exception e) {
                     log.error("【推送元数据实时数据失败】,{}", e);
                 }
-            });
+            });*/
         } catch (Exception e) {
             log.error("元数据实时同步失败 ex:", e);
         } finally {
