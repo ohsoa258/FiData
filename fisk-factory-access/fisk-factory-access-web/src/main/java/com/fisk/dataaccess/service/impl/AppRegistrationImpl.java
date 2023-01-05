260eb74315c9373e02b488fb6e0e386bf700c449
jianwen <jianwen@fisksoft.com.cn>
2023-01-05 16:48:54 +0800

    删除代码

--- fisk-factory-access/fisk-factory-access-web/src/main/java/com/fisk/dataaccess/service/impl/AppRegistrationImpl.java
+++ fisk-factory-access/fisk-factory-access-web/src/main/java/com/fisk/dataaccess/service/impl/AppRegistrationImpl.java
@@ -303,7 +303,6 @@ public class AppRegistrationImpl
         db.displayName = dataSource.dbName;
         db.qualifiedName = data.qualifiedName + "_" + dataSource.id;
         dbList.add(db);
-        data.dbList = new ArrayList<>();
         data.dbList = dbList;
 
         list.add(data);
