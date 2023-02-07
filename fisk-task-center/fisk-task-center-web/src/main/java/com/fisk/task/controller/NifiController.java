4319e028eaa99f62868ff511be054850a17bb3ff
zhuangzhuang@fisksoft.com <zhuangzhuang@fisksoft.com>
2023-02-07 13:30:02 +0800

    系统模块添加系统数据源时设置nifi启动参数接口

--- fisk-task-center/fisk-task-center-web/src/main/java/com/fisk/task/controller/NifiController.java
+++ fisk-task-center/fisk-task-center-web/src/main/java/com/fisk/task/controller/NifiController.java
@@ -2,19 +2,26 @@ package com.fisk.task.controller;
 
 import com.fisk.common.core.response.ResultEntity;
 import com.fisk.common.core.response.ResultEntityBuild;
+import com.fisk.common.core.response.ResultEnum;
+import com.fisk.dataaccess.enums.ComponentIdTypeEnum;
 import com.fisk.datafactory.dto.dataaccess.DataAccessIdDTO;
 import com.fisk.datamodel.vo.DataModelVO;
+import com.fisk.system.dto.datasource.DataSourceSaveDTO;
 import com.fisk.task.dto.daconfig.DataAccessConfigDTO;
 import com.fisk.task.dto.task.NifiCustomWorkListDTO;
 import com.fisk.task.listener.nifi.INifiCustomWorkFlow;
 import com.fisk.task.po.TableNifiSettingPO;
 import com.fisk.task.service.nifi.impl.TableNifiSettingServiceImpl;
+import com.fisk.task.utils.NifiHelper;
 import com.fisk.task.utils.nifi.INiFiHelper;
 import lombok.extern.slf4j.Slf4j;
+import org.omg.CORBA.OBJECT_NOT_EXIST;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author cfk
@@ -68,5 +75,16 @@ public class NifiController {
         return ResultEntityBuild.build(iNifiCustomWorkFlow.suspendCustomWorkNifiFlow(nifiCustomWorkflowId, ifFire));
     }
 
-
+    /**
+     * 添加系统数据源时调用设置nifi参数
+     * @param dto
+     */
+    @PostMapping("/addDataSetParams")
+    public void addDataSetParams(@RequestBody DataSourceSaveDTO dto){
+        Map<String, String> map = new HashMap<>();
+        map.put(ComponentIdTypeEnum.DB_URL.getName(), dto.getConStr());
+        map.put(ComponentIdTypeEnum.DB_USERNAME.getName(), dto.getConAccount());
+        map.put(ComponentIdTypeEnum.DB_PASSWORD.getName(), dto.getConPassword());
+        iNiFiHelper.buildNifiGlobalVariable(map);
+    }
 }
