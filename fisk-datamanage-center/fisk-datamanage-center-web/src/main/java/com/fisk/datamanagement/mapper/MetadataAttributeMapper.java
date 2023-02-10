1bb37409164128c5a5caa36f81096bb1149cdb3d
jianwen <jianwen@fisksoft.com.cn>
2023-02-10 09:56:32 +0800

    元数据属性

---
+++ fisk-datamanage-center/fisk-datamanage-center-web/src/main/java/com/fisk/datamanagement/mapper/MetadataAttributeMapper.java
@@ -0,0 +1,10 @@
+package com.fisk.datamanagement.mapper;
+
+import com.fisk.common.framework.mybatis.FKBaseMapper;
+import com.fisk.datamanagement.entity.MetadataAttributePO;
+
+/**
+ * @author JianWenYang
+ */
+public interface MetadataAttributeMapper extends FKBaseMapper<MetadataAttributePO> {
+}
