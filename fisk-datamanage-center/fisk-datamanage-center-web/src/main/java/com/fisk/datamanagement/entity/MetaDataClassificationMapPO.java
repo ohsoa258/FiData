8b72a9367df5c25590bf5e9443fdb5eb5451ad5a
zhuangzhuang@fisksoft.com <zhuangzhuang@fisksoft.com>
2023-02-14 09:55:41 +0800

    dto置换po

--- fisk-datamanage-center/fisk-datamanage-center-web/src/main/java/com/fisk/datamanagement/entity/MetaDataClassificationMapPO.java
+++ fisk-datamanage-center/fisk-datamanage-center-web/src/main/java/com/fisk/datamanagement/entity/MetaDataClassificationMapPO.java
@@ -1,25 +1,24 @@
 package com.fisk.datamanagement.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.fisk.common.core.baseObject.dto.BaseDTO;
 import com.fisk.common.core.baseObject.entity.BasePO;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
  * @ClassName:
- * @Author: 湖~Tloml
+ * @Author: 湖~Zero
  * @Date: 2023
- * @Copyright: 2023 by 湖~Tloml
+ * @Copyright: 2023 by 湖~Zero
  * @Description:
  **/
-
 @EqualsAndHashCode(callSuper = true)
 @Data
 @TableName(value = "tb_metadata_classification_map")
 public class MetaDataClassificationMapPO extends BasePO {
 
     public Integer metaDataEntityId;
+
 
     public Integer businessClassificationId;
 }
