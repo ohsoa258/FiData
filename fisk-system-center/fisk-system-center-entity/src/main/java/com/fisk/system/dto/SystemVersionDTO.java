13e9225a2a9ccf34e94fa476ae61de0d49850912
sjli <Tenli@fisksoft.com>
2023-04-10 16:23:32 +0800

    代码优化

--- fisk-system-center/fisk-system-center-entity/src/main/java/com/fisk/system/dto/SystemVersionDTO.java
+++ fisk-system-center/fisk-system-center-entity/src/main/java/com/fisk/system/dto/SystemVersionDTO.java
@@ -1,12 +1,6 @@
 package com.fisk.system.dto;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-
-import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
  * @TableName tb_system_version
