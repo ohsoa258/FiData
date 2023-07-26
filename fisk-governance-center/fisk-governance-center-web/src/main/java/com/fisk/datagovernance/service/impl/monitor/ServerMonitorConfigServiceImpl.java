b43d7f5002ec5fde7754f80ebe27c66998c8c9ae
wangjian <wangjian@fisksoft.com>
2023-07-26 17:46:58 +0800

    系统监控新增切换服务器功能及配置服务页面

--- fisk-governance-center/fisk-governance-center-web/src/main/java/com/fisk/datagovernance/service/impl/monitor/ServerMonitorConfigServiceImpl.java
+++ fisk-governance-center/fisk-governance-center-web/src/main/java/com/fisk/datagovernance/service/impl/monitor/ServerMonitorConfigServiceImpl.java
@@ -19,12 +19,16 @@ public class ServerMonitorConfigServiceImpl extends ServiceImpl<ServerMonitorCon
     @Override
     public String getServerMonitorConfig() {
         List<ServerMonitorConfigPO> config = this.list();
-        List<String> result = config.stream().map(i -> {
-            StringBuilder sb = new StringBuilder();
-            sb.append(i.getServerIp()).append(":").append(i.getServerPort()).append(":").append(i.getServerName());
-            return sb.toString();
-        }).collect(Collectors.toList());
-        return result.toString();
+        StringBuilder sb = new StringBuilder();
+        for (ServerMonitorConfigPO serverMonitorConfigPO : config) {
+            sb.append(serverMonitorConfigPO.getServerIp()).append(":").append(serverMonitorConfigPO.getServerPort()).append(":").append(serverMonitorConfigPO.getServerName()).append(" ");
+        }
+//        List<String> result = config.stream().map(i -> {
+//            StringBuilder sb = new StringBuilder();
+//            sb.append(i.getServerIp()).append(":").append(i.getServerPort()).append(":").append(i.getServerName());
+//            return sb.toString();
+//        }).collect(Collectors.toList());
+        return sb.toString();
     }
 
     @Override
