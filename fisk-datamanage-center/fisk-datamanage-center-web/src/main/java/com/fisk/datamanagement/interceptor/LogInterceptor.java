b8f3ef37c15d8f054f4cf0aea20be9870ee630bb
sjli <TenLi@fisksoft.com>
2024-03-15 16:29:02 +0800

    审计日志 ：所有模块添加审计日志（过滤器+拦截器）

---
+++ fisk-datamanage-center/fisk-datamanage-center-web/src/main/java/com/fisk/datamanagement/interceptor/LogInterceptor.java
@@ -0,0 +1,91 @@
+package com.fisk.datamanagement.interceptor;
+
+import com.alibaba.fastjson.JSON;
+import com.fisk.common.core.constants.SystemConstants;
+import com.fisk.common.core.enums.system.AuditServiceTypeEnum;
+import com.fisk.common.framework.jwt.JwtUtils;
+import com.fisk.common.framework.jwt.model.Payload;
+import com.fisk.common.framework.jwt.model.UserDetail;
+import com.fisk.system.client.UserClient;
+import com.fisk.system.dto.auditlogs.AuditLogsDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    @Resource
+    @Lazy
+    private UserClient userClient;
+    @Resource
+    @Lazy
+    private JwtUtils jwtUtils;
+
+    //前置拦截器 记录用户操作日志
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        try {
+            //获取被拦截方法的详情
+            AuditLogsDTO auditLogsDTO = new AuditLogsDTO();
+            auditLogsDTO.setServiceType(AuditServiceTypeEnum.FISK_DATAMANAGE_CENTER.getValue());
+            auditLogsDTO.setRequestType(request.getMethod());
+            auditLogsDTO.setRequestAddr(request.getRequestURI());
+            auditLogsDTO.setIpAddr(request.getRemoteAddr());
+            if ("post".equalsIgnoreCase(auditLogsDTO.getRequestType())
+                    || "put".equalsIgnoreCase(auditLogsDTO.getRequestType())
+                    || "delete".equalsIgnoreCase(auditLogsDTO.getRequestType())
+            ) {
+                auditLogsDTO.setParamMap(JSON.toJSONString(new RequestWrapper(request).getBodyString()));
+            } else {
+                auditLogsDTO.setParamMap(JSON.toJSONString(request.getParameterMap()));
+            }
+
+            //获取请求携带的Token
+            String token = request.getHeader(SystemConstants.HTTP_HEADER_AUTH);
+            if (token != null) {
+                token = token.replace(SystemConstants.AUTH_TOKEN_HEADER, "");
+                //解析token 获取用户信息
+                Payload payload = jwtUtils.parseJwt(token);
+                UserDetail userDetail = payload.getUserDetail();
+                auditLogsDTO.setUserId(userDetail.getId());
+                auditLogsDTO.setUsername(userDetail.getUserAccount());
+            } else {
+                //没有token则是服务间内部远程调用 不记录日志  登录请求暂不记录
+                return true;
+            }
+
+            //记录操作日志
+            userClient.saveAuditLog(auditLogsDTO);
+
+        } catch (Exception e) {
+            log.error("*******************************");
+            log.error("拦截器记录用户操作日志报错=msg：" + e.getMessage());
+            log.error("拦截器记录用户操作日志报错-method name：" + request.getRequestURI());
+            log.error("拦截器记录用户操作日志报错：" + e);
+            log.error("*******************************");
+        }
+        // 继续执行下一个拦截器或处理器
+        return true;
+    }
+
+//    //后置
+//    @Override
+//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+//                           ModelAndView modelAndView) {
+//        // 在请求处理之后执行的逻辑（如果需要）
+//    }
+
+//    //返回
+//    @Override
+//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+//            throws Exception {
+//        // 在整个请求处理完成后执行的逻辑（如果需要）
+//    }
+}
---
+++ fisk-factory-dispatch/fisk-factory-dispatch-web/src/main/java/com/fisk/datafactory/interceptor/LogInterceptorConfig.java
@@ -0,0 +1,39 @@
+package com.fisk.datafactory.interceptor;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class LogInterceptorConfig implements WebMvcConfigurer {
+
+    @Resource
+    private LogInterceptor logInterceptor;
+
+    /**
+     * 配置拦截器要拦截的请求
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                // 拦截所有请求
+                .addPathPatterns("/**")
+                // 排除某些请求，例如静态资源、swagger等
+                .excludePathPatterns(
+                        "/static/**"
+                        , "/css/**"
+                        , "/js/**"
+                        , "/swagger-ui/**"
+                        , "/swagger-ui.html"
+                        , "/swagger-resources/**"
+                        , "/error"
+                        , "/csrf"
+                        , "/"
+                        , "/webjars/**");
+    }
+}
