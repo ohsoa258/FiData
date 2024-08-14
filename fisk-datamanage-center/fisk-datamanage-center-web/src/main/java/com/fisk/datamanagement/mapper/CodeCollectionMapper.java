9c4231a58cdfe034b89fb39159ab5decb767976f
wangjian <wangjian@fisksoft.com>
2024-08-14 15:03:01 +0800

    添加代码集合

--- fisk-datamanage-center/fisk-datamanage-center-web/src/main/java/com/fisk/datamanagement/mapper/CodeCollectionMapper.java
+++ fisk-datamanage-center/fisk-datamanage-center-web/src/main/java/com/fisk/datamanagement/mapper/CodeCollectionMapper.java
@@ -22,6 +22,6 @@ public interface CodeCollectionMapper extends BaseMapper<CodeCollectionPO> {
     Integer getAllCodeCollectionCount();
 
     List<CodeCollectionVO> getCodeCollection(@Param("keyword") String keyword,
-                                             @Param("current") Integer startIndex,
-                                             @Param("size") Integer pageSize);
+                                             @Param("startIndex") Integer startIndex,
+                                             @Param("pageSize") Integer pageSize);
 }
